5563baa950ff063235afd6265d8898dca40ffe3f
siyolisemekuto <103102826+siyolisemekuto@users.noreply.github.com>
2022-06-10 10:00:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-let mark =80
-if(mark > 0 && mark <= 40){
-  console.log('F')
+let mark = 100
+
+if(mark >=0 && mark <= 40){
+  console.log('F');
 }
 else if(mark > 40 && mark <= 50){
-  console.log('E')
+  console.log('E');
 }
 else if(mark > 50 && mark <= 60){
-  console.log('D')
+  console.log('D');
 }
 else if(mark > 60 && mark <= 70){
-  console.log('C')
+  console.log('C');
 }
 else if(mark > 70 && mark <= 80){
-  console.log('B')
+  console.log('B');
 }
  else if(mark > 80 && mark <= 90){
-  console.log('A')
+  console.log('A');
 }
  else if(mark > 90 && mark <= 99){
-  console.log('A+')
+  console.log('A+');
  }
- else if (mark=100){
-  console.log('S')
+ else if(mark == 100){
+  console.log('S');
 }
 else{
-  console.log('invalid mark')
+  console.log('invalid mark');
 }
