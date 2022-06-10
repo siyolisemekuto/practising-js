9c8087e78a7ee0bbb90ae2823fc839a2f88433a7
siyolisemekuto <103102826+siyolisemekuto@users.noreply.github.com>
2022-06-10 08:40:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let mark =80
+if(mark > 0 && mark <= 40){
+  console.log('F')
+}
+else if(mark > 40 && mark <= 50){
+  console.log('E')
+}
+else if(mark > 50 && mark <= 60){
+  console.log('D')
+}
+else if(mark > 60 && mark <= 70){
+  console.log('C')
+}
+else if(mark > 70 && mark <= 80){
+  console.log('B')
+}
+ else if(mark > 80 && mark <= 90){
+  console.log('A')
+}
+ else if(mark > 90 && mark <= 99){
+  console.log('A+')
+ }
+ else if (mark=100){
+  console.log('S')
+}
+else{
+  console.log('invalid mark')
+}
